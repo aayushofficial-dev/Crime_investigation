50646d8145b8c2661a71610e7cd18afe7c75df4a
khadkariwaj090-dotcom <khadkariwaj090@gmail.com>
2026-04-23 21:41:11 +0545

    Deleted unwanted files and finalised room

--- Packages/com.genies.avatar-sdk.client/SDK Core/Runtime/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Genies.Sdk.Avatar")]
-[assembly: InternalsVisibleTo("Genies.Sdk.Avatar.Benchmark")]
-[assembly: InternalsVisibleTo("Genies.Sdk.Avatar.Benchmark.Tests")]
-[assembly: InternalsVisibleTo("Genies.Experience.Core")]
-
